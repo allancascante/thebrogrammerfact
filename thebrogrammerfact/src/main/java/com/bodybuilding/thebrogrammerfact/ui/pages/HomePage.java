cf87e6332ffccb2d3c8ca74b4ae442e5c07b6381
Allan Cascante <allan.cascante@bodybuildingcom>
2012-04-27 16:19:48 -0600

    simple state management with static

--- thebrogrammerfact/src/main/java/com/bodybuilding/thebrogrammerfact/ui/pages/HomePage.java
+++ thebrogrammerfact/src/main/java/com/bodybuilding/thebrogrammerfact/ui/pages/HomePage.java
@@ -4,7 +4,6 @@ import com.bodybuilding.thebrogrammerfact.ui.models.BrogrammerFactsModel;
 import com.bodybuilding.thebrogrammerfact.ui.panels.LatestFactsPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends BasePage {
 	private static final long serialVersionUID = 1L;
