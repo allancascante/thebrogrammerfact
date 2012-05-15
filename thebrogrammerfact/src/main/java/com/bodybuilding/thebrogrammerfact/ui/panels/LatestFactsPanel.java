5e556b24f9d9a51b8e3d9fae0baa85164cd85a88
Allan Cascante <allan.cascante@bodybuilding.com>
2012-05-15 16:54:27 -0600

    Added a list view

--- thebrogrammerfact/src/main/java/com/bodybuilding/thebrogrammerfact/ui/panels/LatestFactsPanel.java
+++ thebrogrammerfact/src/main/java/com/bodybuilding/thebrogrammerfact/ui/panels/LatestFactsPanel.java
@@ -1,9 +1,12 @@
 package com.bodybuilding.thebrogrammerfact.ui.panels;
 
+import com.bodybuilding.thebrogrammerfact.entities.BrogrammerFact;
 import com.bodybuilding.thebrogrammerfact.ui.models.BrogrammerFactsModel;
 import com.bodybuilding.thebrogrammerfact.ui.pages.AddAFactPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
@@ -22,7 +25,14 @@ public class LatestFactsPanel extends Panel {
         final BrogrammerFactsModel factsModel = (BrogrammerFactsModel)getDefaultModel();
 
         Random random = new Random();
-        add(new Label("lastBrogrammerFactLabel", new PropertyModel<String>(factsModel.getObject().get(random.nextInt(factsModel.getObject().size())), "fact")));
+        add(new ListView<BrogrammerFact>("lastBrogrammerFacts", factsModel) {
+            @Override
+            protected void populateItem(ListItem<BrogrammerFact> brogrammerFactListItem) {
+                Label lastBrogrammerFact = new Label("lastBrogrammerFact", brogrammerFactListItem.getModelObject().getFact());
+                lastBrogrammerFact.setRenderBodyOnly(true);
+                brogrammerFactListItem.add(lastBrogrammerFact);
+            }
+        });
         add(new BookmarkablePageLink<AddAFactPage>("addNewFactLink", AddAFactPage.class));
     }
 
